28117910f859e0467c82e19ceeb6a6d673ac5f98
Azra <azraawan@yahoo.com>
2021-04-11 15:45:21 -0400

    Fix: Adjusted Code for app.py file

--- app/app.py
+++ app/app.py
@@ -25,8 +25,14 @@ def cities_import() -> List[Dict]:
 
     return result
 
-
 @app.route('/')
+def index():
+    user = {'username': 'Miguel'}
+    cities_data = cities_import()
+
+    return render_template('index.html', title='HOME', user=user, cities=cities_data)
+
+@app.route('/api/cities')
 def index() -> str:
     js = json.dumps(cities_import())
     resp = Response(js, status=200, mimetype='application/json')
