34095d65ca51f93519c18ca5b943186a3a75a411
Azra <azraawan@yahoo.com>
2021-04-19 11:59:51 -0400

    Fix: Edit Code in init.sql database to 'tableCitiesImport'

--- db/init.sql
+++ db/init.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE citiesData;
 use citiesData;
 
-CREATE TABLE IF NOT EXISTS tblCitiesImport (
+CREATE TABLE IF NOT EXISTS 'tblCitiesImport' (
     `id` int AUTO_INCREMENT,
     `fldName` VARCHAR(21) CHARACTER SET utf8,
     `fldLat` NUMERIC(6, 4),
@@ -12,7 +12,7 @@ CREATE TABLE IF NOT EXISTS tblCitiesImport (
     `fldPopulation` INT,
     PRIMARY KEY (`id`)
 );
-INSERT INTO tblCitiesImport (fldName,fldLat,fldLong,fldCountry,fldAbbreviation,fldCapitalStatus,fldPopulation) VALUES
+INSERT INTO 'tblCitiesImport' (fldName,fldLat,fldLong,fldCountry,fldAbbreviation,fldCapitalStatus,fldPopulation) VALUES
     ('Tokyo',35.685,139.7514,'Japan','JPN','primary',35676000),
     ('New York',40.6943,-73.9249,'United States','USA','NA',19354922),
     ('Mexico City',19.4424,-99.131,'Mexico','MEX','primary',19028000),
