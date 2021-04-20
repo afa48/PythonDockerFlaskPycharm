f036cf0ac57aef112b70bf608a917a5c88edae48
Azra <azraawan@yahoo.com>
2021-04-20 15:44:14 -0400

    Task: Removed Init.sql file, Added crash.sql file, Removed screenshots

---
+++ db/crash_catalonia.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS crash_catalonia (
+    `Day_of_Week` VARCHAR(9) CHARACTER SET utf8,
+    `Number_of_Crashes` INT
+);
+INSERT INTO crash_catalonia VALUES
+    ('Sunday',13664),
+    ('Monday',17279),
+    ('Tuesday',17337),
+    ('Wednesday',17394),
+    ('Thursday',17954),
+    ('Friday',19147),
+    ('Saturday',15714);
